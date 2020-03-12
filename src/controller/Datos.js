19c8a54bc1c932850f36bc9ce28cf33359fec393
f3rn4nd001 <f3rn4nd0.v3ntur4@gmail.com>
2020-03-11 21:19:57 -0600

    segundos

--- src/controller/Datos.js
+++ src/controller/Datos.js
@@ -7,9 +7,7 @@ DatosSensores.getMTDatos = async(req, res) => {
 
     const flama = await Sensores.find().sort({ fecha: 'desc' }).where('nombre').equals('Flama');
 
-    const sensores = await Sensores.find({ data: 'Apartado' });
-    console.log(sensores);
-
+    const sensores = await Sensores.find();
 
     res.render('principal/index', { sensores, flama });
     console.log(flama);
